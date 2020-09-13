d2a19189520085ca10a20e87247e732f09233ac1
CanniJr <cannigialaluw@gmail.com>
2020-09-13 04:08:48 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+
+const writers = ["Lisa", "Brendan", "Ali"];
+const event = "birthday";
+let newArray = []
+// for loop format
+// for ([initialization]; [condition]; [iteration]) {
+//     [loop body]
+//   }
+  
+function writeCards(writers, event) {
+    for (let i = 0; i < writers.length; i++) {
+       newArray.push(`Thank you, ${writers[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArray
+}
+
+
+
+function countDown(number) {
+    let i = 0
+
+    while (i < number + 1){
+        console.log(number - i);
+        i++;
+    }
+}
